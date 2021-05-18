56779e2da0b84b408ac9c281d7937b8f4fe43b23
HoduGwaja <honeybutter9250@gmail.com>
2021-05-18 15:55:29 +0900

    [Incident-Tracker | 2021.05.18 | fixed bug on setting DataBase]

--- models/index.js
+++ models/index.js
@@ -1,13 +1,10 @@
-const { Sequelize } = require("sequelize/types");
-const env = process.env.NODE_ENV || 'devleopment';
-const config = require = ('../config/config')[env];
-const db = {};
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
 
+const db = {};
 const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config,
+  config.database, config.username, config.password, config,
 );
 
 db.sequelize = sequelize;
@@ -15,21 +12,45 @@ db.Sequelize = Sequelize;
 db.User = require('./user')(sequelize, Sequelize);
 db.Post = require('./post')(sequelize, Sequelize);
 db.Hashtag = require('./hashtag')(sequelize, Sequelize);
-db.User.hashMany(db.Post);
-db.Post.belongs(db.User);
+db.User.hasMany(db.Post);
+db.Post.belongsTo(db.User);
 db.Post.belongsToMany(db.Hashtag, {through : 'PostHashtag'});
-db.Hashtag.belongsToMany(db.Post, {hrough : 'PostHashTag'});
+db.Hashtag.belongsToMany(db.Post, {through : 'PostHashtag'});
 
-db.User.belongs('db.User', {
+db.User.belongsToMany(db.User, {
   foreignKey : 'followingId',
   as : 'Followers',
   through : 'Follow',
 });
 
-db.User.belongsToMany('db.User', {
+db.User.belongsToMany(db.User, {
   foreignKey : 'followingId',
   as : 'Followings',
   through : 'Follow',
 });
 
 module.exports = db;
+
+/*
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.User = require('./user')(sequelize, Sequelize);
+db.Post = require('./post')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+
+db.User.hasMany(db.Post);
+db.Post.belongsTo(db.User);
+db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });
+db.User.belongsToMany(db.User, { foreignKey: 'followingId', as: 'Followers', through: 'Follow' });
+db.User.belongsToMany(db.User, { foreignKey: 'followerId', as: 'Followings', through: 'Follow' });
+
+module.exports = db;
+*/
